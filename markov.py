f80548a0752bc4b892741093f5e91e4097c37931
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-01 12:59:13 -0800

    add n-gram functionality

--- markov.py
+++ markov.py
@@ -2,7 +2,7 @@ from random import choice
 import sys
 
 
-def open_and_read_file(file_path, file_path2):
+def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
 
     Takes a string that is a file path, opens the file, and turns
@@ -10,7 +10,7 @@ def open_and_read_file(file_path, file_path2):
     """
 
     text = open(file_path).read()
-    text += open(file_path2).read()
+    # text += open(file_path2).read()
 
     return text
 
@@ -48,6 +48,39 @@ def make_chains(text_string):
     return chains
 
 
+def make_n_chains(text_string, n):
+    """Takes input text as string; returns _dictionary_ of markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2.., wordn)
+    and the value would be a list of the word(s) that follow those words
+    in the input text.
+
+    For example:
+
+        >>> make_chains("hi there mary hi there juanita")
+        {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
+    """
+
+    chains = {}
+    words = text_string.split()
+
+    start_index = 0
+    end_index = n - 1
+    value_index = n
+
+    while value_index < len(words):
+        word_tuple = tuple(words[start_index:end_index + 1])
+        value = words[value_index]
+        chains[word_tuple] = chains.get(word_tuple, []) + [value]
+        start_index += 1
+        end_index += 1
+        value_index += 1
+
+    chains[tuple(words[start_index:end_index + 1])] = [""]
+    print chains
+    return chains
+
+
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
@@ -70,21 +103,53 @@ def make_text(chains):
         text += word_pair[0] + " "
         next_word = choice(chains[word_pair])
         word_pair = (word_pair[1], next_word)
-    
+
     text += word_pair[0] + " " + word_pair[1]
     return text
 
 
+def make_n_text(chains):
+    """Takes dictionary of markov chains; returns random text."""
+
+    text = ""
+    while True:
+        word_tuple = choice(chains.keys())
+        if word_tuple[0][0].isupper() is True:
+            break
+
+    end_punctuation = [".", "!", "?"]
+    while True:
+        end_word_tuple = choice(chains.keys())
+        if end_word_tuple[-1][-1] in end_punctuation:
+            break
+
+    while word_tuple != end_word_tuple:
+        if word_tuple[-1] == "":
+            break
+
+        text += str(word_tuple[0]) + " "
+        next_word = choice(chains[word_tuple])
+        word_tuple = word_tuple[1:] + (next_word,)
+
+    text += word_tuple[0] + " " + word_tuple[1]
+    return text
+
+
+
 input_path = sys.argv[1]
-input_path_2 = sys.argv[2]
+# input_path_2 = sys.argv[2]
 
 # Open the file and turn it into one long string
-input_text = open_and_read_file(input_path, input_path_2)
+input_text = open_and_read_file(input_path)
 
-# Get a Markov chain
-chains = make_chains(input_text)
+# set n to a number
+n = 3
+
+# # Get a Markov chain
+chains = make_n_chains(input_text, n)
+
 
 # Produce random text
-random_text = make_text(chains)
+random_text = make_n_text(chains)
 
 print random_text
